02eff48f8a2021cd662575d6aec83a67978fe637
M.TAHA <112281071+TAHA2255@users.noreply.github.com>
2022-10-10 05:31:26 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+let nav = document.getElementById("side");
+function myFunction(x) {
+    x.classList.toggle("change");
+    nav.classList.toggle("navbartogg");
+}
+let home = document.getElementById("red");
+let home2 = document.getElementById("fed");
+let home3 = document.getElementById("nr");
+
+home.style.color = 'red';
+home2.style.color = 'red';
+home3.style.color = 'red';
+// // let str = home.toString();
+// home.style.color = "red";
+ 
